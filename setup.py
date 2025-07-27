688a8aaf0ac4fd2f6c43658d960c4b1f7af87ab1
Suyash Matade <suyashmatade007@gmail.com>
2025-07-27 14:58:52 +0530

    Project Structure Ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Suyash Matade",
+    author_email="suyashmatade007@gmail.com",
+    packages=find_packages()
+)
