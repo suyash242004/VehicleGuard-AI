e9af979fd9fa07fbbd97a1dd8098ec323201527f
Suyash Matade <suyashmatade007@gmail.com>
2025-07-28 11:46:06 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -23,7 +23,7 @@
 
 # --------------------------------------------------------------------------------
 
-# from src.pipline.training_pipeline import TrainPipeline
+from src.pipline.training_pipeline import TrainPipeline
 
-# pipline = TrainPipeline()
-# pipline.run_pipeline()
+pipline = TrainPipeline()
+pipline.run_pipeline()
