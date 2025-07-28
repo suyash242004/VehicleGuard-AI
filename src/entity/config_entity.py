534878512f884383c4d7795889a7fd87f24c54fe
Suyash Matade <suyashmatade007@gmail.com>
2025-07-28 19:30:24 +0530

    ready till model pusher

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -45,7 +45,7 @@ class ModelTrainerConfig:
     trained_model_file_path: str = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILE_NAME)
     expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
     model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
-    _n_estimators = MODEL_TRAINER_N_ESTIMATORS
+    _n_estimators = MODEL_TRAINER_N_ESTIMATORS      
     _min_samples_split = MODEL_TRAINER_MIN_SAMPLES_SPLIT
     _min_samples_leaf = MODEL_TRAINER_MIN_SAMPLES_LEAF
     _max_depth = MIN_SAMPLES_SPLIT_MAX_DEPTH
